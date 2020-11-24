6637bd7b9765a501a0b377fd1b57ac57d2acaefd
Paulo Ricardo <pauloricardoh@hotmail.com>
2020-11-24 16:10:25 -0300

    Migrations

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Produto.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Produto.cs
@@ -5,12 +5,13 @@
         public int Id { get; internal set; }
         public string Nome { get; internal set; }
         public string Categoria { get; internal set; }
-        public double Preco { get; internal set; }
+        public double PrecoUnidade { get; internal set; }
+        public string Unidade { get; internal set; }         
 
 
         public override string ToString()
         {
-            return $"Produto : {this.Id},{this.Nome},{this.Categoria},{this.Preco}";
+            return $"Produto : {this.Id},{this.Nome},{this.Categoria},{this.PrecoUnidade}";
         }
 
     }
--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -14,69 +14,9 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            using ( var contexto = new LojaContext())
+            using (var contexto = new LojaContext())
             {
-                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
-                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
-                loggerFactory.AddProvider(SqlLoggerProvider.Create());
 
-                var produtos = contexto.Produtos.ToList();
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                //ADDed - Unchamged
-                //var novoProduto = new Produto()
-                //{
-                //    Nome = "Desinfetante",
-                //    Categoria = "Limpeza",
-                //    Preco = 2.99
-                //};
-
-                //contexto.Produtos.Add(novoProduto);
-
-                //ExibeEntries(contexto.ChangeTracker.Entries());
-
-                //contexto.SaveChanges();
-
-                //ExibeEntries(contexto.ChangeTracker.Entries());
-
-               
-                //UPDATE-Modified
-                //var p1 = produtos.Last();
-                //p1.Nome = "Percy Jackson o Ladrão de Raios";
-
-                //Console.WriteLine("=================");
-                //foreach (var e in contexto.ChangeTracker.Entries())
-                //{
-                //    Console.WriteLine(e);
-                //}
-
-                //contexto.SaveChanges();
-
-
-                //DELETED
-                var p1 = produtos.First();
-                contexto.Produtos.Remove(p1);
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                //DETACHED
-                contexto.SaveChanges();
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                var entry = contexto.Entry(p1);
-                Console.WriteLine("\n\n" + entry.Entity.ToString() + " - " + entry.State);
-
-            }
-        }
-
-        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
-        {
-            Console.WriteLine("=================");
-            foreach (var e in entries)
-            {
-                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
             }
         }
     }
