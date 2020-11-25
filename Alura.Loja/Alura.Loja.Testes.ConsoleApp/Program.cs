349eca4f3c316a28be061701e7fe250a1102df92
Paulo Ricardo <pauloricardoh@hotmail.com>
2020-11-25 16:42:56 -0300

    1 para N

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -14,10 +14,44 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            using (var contexto = new LojaContext())
-            {
+                var paoFrances = new Produto();
+                paoFrances.Nome = "Pão";
+                paoFrances.PrecoUnidade = 0.40;
+                paoFrances.Unidade = "Unidade";
+                paoFrances.Categoria = "Padaria";
 
+                var compra = new Compra();
+                compra.Quantidade = 6;
+                compra.Produto = paoFrances;
+                compra.Preco = paoFrances.PrecoUnidade * compra.Quantidade;
+
+
+            using(var contexto = new LojaContext())
+            {
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+
+                contexto.Compras.Add(compra);
+
+
+                ExibeEntries(contexto.ChangeTracker.Entries());
+
+                contexto.SaveChanges();
+
+            }     
+        }
+
+        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
+        {
+            foreach (var e in entries)
+            {
+                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
             }
         }
+
+
+
     }
 }
